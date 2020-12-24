987a2294522284b8d950f0b7b5fd31790b7ec635
DanielXavier114 <danielsxavier7@gmail.com>
2020-12-24 06:44:25 +0000

    compressao RLE e respetiva taxa

--- modulo_a/mainA.c
+++ modulo_a/mainA.c
@@ -6,6 +6,7 @@
 int main(){
 clock_t begin = clock();
 
+
     unsigned long long total;
     long long n_blocks;
     unsigned long size_of_last_block, block_size;
@@ -29,9 +30,14 @@ clock_t begin = clock();
     printf("\n File: bbb.zip");
     printf("\n %ld blocks of %lu bytes + 1 block of %lu bytes", n_blocks-1, block_size, size_of_last_block);
     printf("\n File size = %llu bytes\n", total);
+
+moduloA("aaa.txt",block_size, total, n_blocks);
+clock_t end = clock();
+double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+ 
+
     
-   clock_t end = clock();
-   double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+   
 
     printf ("\n Daniel e Isabela, a93292, MIEI/CD, 21-JAN-2021 ");
     printf ("\n Módulo: f (cálculo das frequências dos símbolos");
@@ -41,6 +47,6 @@ clock_t begin = clock();
     printf("\n Tamanho dos blocos analisados no ficheiro RLE: ", block_size);
     printf("\n Tempo de execução do módulo (milissegundos): %d ", time_spent);
     printf("\n Ficheiros gerados: ");
-
+**/
     return(0);
 }
--- modulo_a/modulo_a.c
+++ modulo_a/modulo_a.c
@@ -7,7 +7,7 @@
 
 
 //Função que devolve o tamanho do bloco para análise
-int tamanho_bloco (char t){
+int funcaotamanho_bloco (char t){
     int tamanho;
 
     if (t=='K') tamanho= 655360;
@@ -16,7 +16,7 @@ int tamanho_bloco (char t){
 }
 
 //Função que devolve o número de bytes do ficheiro
-long tamanho_ficheiro(const char* ficheiro)
+long funcaotamanho_ficheiro(const char* ficheiro)
 {
     long tamanho;
     FILE *f;
@@ -31,39 +31,99 @@ long tamanho_ficheiro(const char* ficheiro)
 }
 
 //Função que comprime o ficheiro
-int compressao (char* ficheiro){
+int compressao (char* ficheiro, char* lista, unsigned long tamanhobloco, int bloco){
+
+char nomeFicheiro[100];
+strcpy (nomeFicheiro,ficheiro);
+char extensaoRLE = ".rle";
+strcat(nomeFicheiro, extensaoRLE);
+
+   
+FILE *fp = fopen("aaa.txt", "ab");
+if (fp == NULL) printf ("Erro abrir ficheiro");
+
+fseek(fp, (tamanhobloco*bloco),SEEK_SET);
+int i =0;
+char atual = lista[i];
+char anterior;
+int c= 0;
+int carateres = 0;
+unsigned char novoContador;
+while (lista[i]) {
+   anterior = atual;
+   i++;
+   atual = lista [i];
+   
+   if (atual == anterior) c++;
+   else {
+       if (c <= 3){
+           while (c != 0) {
+               fputc(anterior, fp);
+               c--;
+               carateres++;
+
+           }
+           contador = 1;
+       } else {
+           fputc ('\0', fp);
+           fprintf (fp, "%c", anterior);
+           novoContador = c;
+           fputc (novoContador, fp);
+           carateres += 3;
+           c = 1;
+       }
+
+   }
+}
+fclose(fp);
+int j,crle = 0;
+int taxa;
+while (lista [i]){
+j++;
+crle;
+}
+taxa= (((abs(crle-carateres))/crle)*100);
+return taxa;
+}
 
 
-    unsigned long long total;
-    long long n_blocks;
-    unsigned long size_of_last_block, block_size;
-    FILE *fp;
 
-    // Using function fsize() when file is already opened
+char* novaLista (char* ficheiro, unsigned long tamanhobloco, int bloco, unsigned long long tamanhoficheiro){
+    int i = 0;
+ char* lista = malloc(sizeof(char)*tamanho_bloco);
+ FILE* fp = fopen(ficheiro, "rb");
+ if (fp== NULL) printf ("Erro abrir ficheiro");
+ fseek(fp,( tamanho_bloco * bloco), SEEK_SET);
+ fread (lista, tamanho_bloco,1,fp);
+ lista[tamanho_bloco] = '\0';
+
+ return lista;
+}
+
+
+
+
+
+
+
+
+   int moduloA (char* ficheiro, unsigned long tamanhobloco, unsigned long long tamanho_ficheiro, long long numeroblocos){
+       if (tamanho_ficheiro <= 1024){
+           printf ("Ficheiro demasiado pequeno");
+           return 0;
+       }
+       int bloco = 0;
+       char* lista = novaLista(ficheiro, tamanhobloco, bloco, tamanho_ficheiro);
+       
+    int percentagem = compressao (ficheiro,lista, tamanhobloco, bloco);
     
-    block_size = 2048;
-    n_blocks = fsize(fp, NULL, &block_size, &size_of_last_block);
-    total = (n_blocks-1) * block_size + size_of_last_block;
-  
-   int c;
-   int n = 0;
-   int i;
-  
-   fp = fopen("file.txt","r");
-   if(fp == NULL) {
-      printf("Error in opening file");
-      return(-1); }
-   while (c = fgetc(fp) != '\0'){
-    char anterior =c;
-    i++;
-    
-
-
    }
       
       
 
    fclose(fp);
-   return(0);
+   return 0;
 }
+
+
 
