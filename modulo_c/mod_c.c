8ef4fe0b11f54038bce810da2a97a9fef88b1dcb
99ita <eduardojafaraujo@gmail.com>
2020-12-27 18:58:44 +0000

    mod_cv1

---
+++ modulo_c/mod_c.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h> 
+#include <time.h> 
+
+
+
+
+/*
+if((file=fopen("C:\\Users\\ste\\Desktop\\file.txt", "r"))==NULL) 
+{
+    printf("Cannot open file.\n");
+    puts(strerror(errno));
+
+    getchar();
+    exit(1);
+}*/
+
+
+
+int bytearr[8]; 
+
+
+//Tranforma o bytearray no byte(char) coorespondente; 
+char bytearr_to_byte(){
+	int res = 0;
+	for(int i = 0; i < 8; i++){
+		res += bytearr[i] * (pow(2,(double) i));
+	}
+	return (char) res;
+}
+
+
+
+
+void print_char_file(char c){
+    
+    char *file = "teste.txt";
+
+
+    FILE *fp;
+    if((fp=fopen("file.txt", "w+"))==NULL) {
+        printf("Impossivel abrir ficheiro.\n");
+        puts(strerror(errno));
+
+        exit(1);
+    }
+
+    fputs("abcdef",fp);
+    fclose(fp);
+}
+
+
+
+
+
+
+
+int main(){
+
+
+    bytearr[0] = 0;  //1
+    bytearr[1] = 1;  //2
+    bytearr[2] = 1;  //4
+    bytearr[3] = 1;  //8
+    bytearr[4] = 1;  //16
+    bytearr[5] = 0;  //32
+    bytearr[6] = 0;  //64
+    bytearr[7] = 0;  //128
+
+    char c = bytearr_to_byte();
+    print_char_file(c);
+
+
+    printf("%c\n", c);
+
+	return 0;
+}
