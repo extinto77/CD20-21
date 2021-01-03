84a286f4b1fdbe2c16a4a589d18100facae848f9
DanielXavier114 <danielsxavier7@gmail.com>
2021-01-03 23:36:53 +0000

    modulo A

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 shafa=shafa.c
 modulo_b=modulo_b/modulo_t.c
+modulo_a=modulo_a/modulo_a.c
 debug=-g
 #errors=-std=gnu11 -Wall -Wextra -pedantic-errors
 
 all:
-	gcc $(debug) -o shafa $(shafa) $(modulo_b) 
+	gcc $(debug) -o shafa $(shafa) $(modulo_b) $(modulo_a)
--- shafa.c
+++ shafa.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "modulo_b/modulo_t.h"
+#include "modulo_a/modulo_a.h"
 /* 
 #include "modulo_a/modulo_.h"
 #include "modulo_c/modulo_.h"
@@ -18,23 +19,40 @@ int main(int argc, char *argv[]){
     char *arg1 = argv[2];
     char modulo = *argv[3];
     
+
+    char forceRLE = '0';
+    char tamanhoBloco = 'D';
+
+
+    int i;
+    for(i = 2; i < argc; ++i){
+        if(argv[i][1] == 'c'){
+            forceRLE = '1'; ++i; 
+        }
+        else if(argv[i][1] == 'b'){
+            ++i;
+            switch(argv[i][0]){
+                case 'K':
+                    tamanhoBloco = 'K';
+                    break;
+                case 'm':
+                    tamanhoBloco = 'm';
+                    break;
+                case 'M':
+                    tamanhoBloco = 'M';
+                    break;
+                default:
+                    tamanhoBloco = 'D';
+            }
+       
+        }
+    }
+
     if(strcmp(arg1, "-m") == 0){
         switch (modulo){
         case 'f':
-            char tam = 'D';
-
-            if (argc>6) {
-                tam = argv[5];
-                if ( strcmp(argv[6],"-c") && strcmp(argv[7],"r") ) moduloA(filename, 1, tam);
-                else moduloA(filename, 0, tam);
-            }
-            if (argc>4){
-                if ( strcmp(argv[4],"-b") ){
-                    tam = argv[5];
-                    moduloA(filename, 0, tam);
-                }
-                if ( strcmp(argv[4],"-c") && strcmp(argv[5],"r") ) moduloA(filename, 1, tam);
-            }
+            moduloA(filename, forceRLE, tamanhoBloco);
+      
             break;
 
         case 't':
